d43c59b60a2a44d4d213ad523d260565bc438ca1
Yangwoo Cho <101324787+erv2bh@users.noreply.github.com>
2024-09-01 22:52:46 +0900

    Time: 94 ms (67.98%), Space: 56 MB (32.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -4,14 +4,17 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-  const dp = new Array(amount + 1).fill(Infinity);
+  let dp = Array(amount + 1).fill(Infinity);
+  
   dp[0] = 0;
-
-  for (let coin of coins) {
-    for (let i = coin; i <= amount; i++) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+  
+  for (let i = 1; i <= amount; i++) {
+    for (let coin of coins) {
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
     }
   }
-
+  
   return dp[amount] === Infinity ? -1 : dp[amount];
 };
